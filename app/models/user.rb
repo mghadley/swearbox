b7a2c8158dcfe47aa3e0270727105991b27c0416
Richi Priego <rpriego@verisys.com>
2016-10-22 17:15:35 -0600

    fixes merge conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,11 @@
 class User < ApplicationRecord
-	after_save :crawl_github
-
-	# may not need will test tomorrow
-  def self.create_with_omniauth(auth)
-    create! do |user|
-      user.provider = auth["provider"]
-      user.uid = auth["uid"]
-      user.name = auth["info"]["name"]
-    end
-  end
+  include GithubApi
+  after_save :crawl_github
 
   def crawl_github
-  	if self.github_username && !self.crawler_started
-	  	self.update(crawler_started: true)
-	  	CrawlerWorker.perform_async(self.id)
-	  end
+    if self.github_username && !self.crawler_started
+    self.update(crawler_started: true)
+    CrawlerWorker.perform_async(self.id)
+    end
   end
 end
