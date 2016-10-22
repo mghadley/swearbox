2709d1aa464ac980bd5ac4b7f399b1b52db2ec18
mghadley <matthew.g.hadley@gmail.com>
2016-10-22 00:36:51 -0600

    maybe finished sin counting

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'jbuilder', '~> 2.5'
 gem "github_api"
 gem 'httparty'
 gem 'multi_xml'
+gem 'sidekiq'
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,2 @@
 class User < ApplicationRecord
-	include GithubApi
-
-	def count_sins(strings)
-		regex_hashes = Swearword.all.map { |sw| { id: sw.id, regex: Regexp.new("\\b#{sw.word}\\b") } }
-		regex_hashes.each do |hash|
-			strings.each do |string|
-				count = string.scan(hash[:regex]).length
-				sin = Sin.find_or_initialize_by(user_id: self.id, swearword_id: hash[:id])
-				if count > 0
-					sin.count += count
-					sin.save
-				end
-			end
-		end
-	end
 end
