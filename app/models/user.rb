cb0126a2bddd6ad57e2485c17f7905252d52816b
mghadley <matthew.g.hadley@gmail.com>
2016-10-22 23:26:27 -0600

    spaces > tabs

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
 
   def crawl_github
     if self.github_username && !self.crawler_started
-	    self.update(crawler_started: true)
-	    CrawlerWorker.perform_async(self.id)
+      self.update(crawler_started: true)
+      CrawlerWorker.perform_async(self.id)
     end
   end
 end
