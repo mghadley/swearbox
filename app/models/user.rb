3d72a7d54c551bf714be4325e6f8f6ca44c7b6ea
mghadley <matthew.g.hadley@gmail.com>
2016-10-23 13:09:37 -0600

    testing app without background job

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,14 @@ class User < ApplicationRecord
   has_many :sins
   has_many :swearwords, through: :sins
 
-  after_save :crawl_github
+  # after_save :crawl_github
 
-  def crawl_github
-    if self.github_username && !self.crawler_started
-      self.update(crawler_started: true)
-      CrawlerWorker.perform_async(self.id)
-    end
-  end
+  # def crawl_github
+  #   if self.github_username && !self.crawler_started
+  #     self.update(crawler_started: true)
+  #     CrawlerWorker.perform_async(self.id)
+  #   end
+  # end
 
   def self.top_ten
     arr = User.all.sort { |x,y| x.total_owed <=> y.total_owed}
